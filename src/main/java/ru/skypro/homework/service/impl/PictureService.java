4868abfc6b4161356ef0de16944ce59f82c34487
Igor <avt191540@yandex.ru>
2022-08-11 17:38:44 +0300

    add functionality of pictures and different corrections continue-3

--- src/main/java/ru/skypro/homework/service/impl/PictureService.java
+++ src/main/java/ru/skypro/homework/service/impl/PictureService.java
@@ -2,6 +2,10 @@ package ru.skypro.homework.service.impl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import ru.skypro.homework.dto.PictureDto;
@@ -14,6 +18,8 @@ import ru.skypro.homework.repo.PictureRepository;
 
 import javax.transaction.Transactional;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -42,6 +48,21 @@ public class PictureService {
         picture.setData(pictureFile.getBytes());
         pictureRepository.save(picture);
         return pictureMapper.pictureToPictureDto(picture, ads);
+    }
+
+    public List<ResponseEntity<byte[]>> downloadPictures(Long idAds) {
+        List<Picture> pictures = pictureRepository.findAllByAds_Id(idAds);
+        if (pictures.size() == 0) {
+            Throwable throwable = new NotFoundException("Для объявления с идентификатором " + idAds + " картинок нет.");
+        }
+        ArrayList<ResponseEntity<byte[]>> listPictures = new ArrayList<ResponseEntity<byte[]>>();
+        for (Picture picture : pictures) {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.parseMediaType(picture.getMediaType()));
+            headers.setContentLength(picture.getData().length);
+            listPictures.add(ResponseEntity.status(HttpStatus.OK).headers(headers).body(picture.getData()));
+        }
+        return listPictures;
     }
 
     public Picture findPictureById(Long idPicture) throws NotFoundException {
